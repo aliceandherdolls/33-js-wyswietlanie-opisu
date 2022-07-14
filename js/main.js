caaa8bfac49801663c974d2cacf3d368bc7f1411
aliceandherdolls <alicjagorkiewicz92@gmail.com>
2022-07-14 14:31:06 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let paragraph = document.getElementById("opis");
+
+console.log(paragraph);
+
+
+
+
+let button = document.querySelector('button');
+
+button.onclick = function () {
+    document.getElementById("opis").innerText = "To jest bardzo dobry produkt i ma świetny opis";
+
+}
+
+
+console.log(button);
